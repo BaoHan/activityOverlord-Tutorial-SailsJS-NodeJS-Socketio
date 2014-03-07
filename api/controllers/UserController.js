ce62a48d97a9f5f633656e24df448b0cc84daeab
pierreburgy <pierreburgy@gmail.com>
2014-03-07 17:41:34 +0100

    UPDATE episode 5

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -25,14 +25,24 @@ module.exports = {
    * (specific to UserController)
    */
   'new': function (req,res) {
-      res.view();
+    res.locals.flash = _.clone(req.session.flash);
+    res.view();
+    req.session.flash = {};
   },
 
   create: function  (req, res, next) {
       User.create(req.params.all(), function userCreated (err, user) {
-          if (err) return next(err);
+          // if (err) return next(err);
+          if (err) {
+            console.log(err);
+            req.session.flash = {
+                err: err
+            }
+            return res.redirect('/user/new');
+          };
 
           res.json(user);
+          req.session.flash = {};
       })
   }
 
